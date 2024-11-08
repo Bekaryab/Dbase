2d953264b49782965118d762bbfcf3a3057a6f7e
Bekarys <abievb91@gmail.com>
2024-11-08 21:53:12 +0500

    data-base

--- console_11.sql
+++ console_11.sql
@@ -57,3 +57,89 @@ select * from librarians;
 
 select count(librarian_id) as count_librarian_id
 from librarians;
+
+--Пример BETWEEN
+
+--Следующая инструкция SQL выбирает все продукты с ценой от 10 до 20:
+
+select * from members
+where librarian_id between 2001 and 2003;
+
+select * from members;
+
+--Чтобы отобразить продукты вне диапазона предыдущего примера, используйте NOT BETWEEN:
+
+select * from members
+where librarian_id not between 2001 and 2003;
+
+
+--В следующей инструкции SQL выбираются все продукты с ценой от 10 до 20. Кроме того, не показывайте продукты с категорией ID 1,2 или 3:
+select * from members
+where librarian_id between 2003 and 2005
+and not member_id in (1004, 1006, 1007);
+
+select * from members;
+
+
+--Пример - использование условия BETWEEN со значениями даты
+
+select * from borrowings
+where return_date between '2021-05-20' and '2023-05-20';
+
+select * from borrowings;
+
+
+
+
+--AVG
+--средный значение
+select avg(member_id)
+from members;
+
+--mинимум число
+select min(member_id)
+from members;
+
+
+
+
+
+--LIKE
+
+select member_name
+from members
+where member_name LIKE 'A%';
+
+--Следующая инструкция SQL выбирает всех клиентов с именем клиента, оканчивающимся на "a":
+select member_name
+from members
+where member_name like '%h';
+
+select * from members;
+
+
+
+
+
+
+
+--JOIN
+--Join — оператор, который используют, чтобы объединять строки из двух или более таблиц на основе связующего столбца между ними.
+select members.member_id, borrowings.borrowing_id
+from members
+join borrowings on members.member_id = borrowings.member_id;
+
+
+
+
+-- SELECT members.member_name, borrowings.return_date, librarians.city — выбираем имя участника (member_name), дату возврата (return_date) и город библиотекаря (city).
+--
+-- FROM members — начинаем с таблицы members.
+--
+-- JOIN borrowings ON members.member_id = borrowings.member_id — соединяем таблицу members с borrowings по member_id, чтобы получить данные о выдачах для каждого члена библиотеки.
+--
+-- JOIN librarians ON librarians.librarian_id = borrowings.librarian_id — соединяем таблицу librarians с borrowings по librarian_id, чтобы получить информацию о библиотекаре, который выдал книгу.
+select members.member_name, borrowings.return_date, librarians.city
+from members
+join borrowings on members.member_id = borrowings.member_id
+join librarians on librarians.librarian_id = borrowings.librarian_id;
